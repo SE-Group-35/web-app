b04484748c20ed4e2f9b0dd0c9cf4a48ab9c6980
Ridmi97 <67148053+Ridmi97@users.noreply.github.com>
2021-10-24 19:33:40 +0530

    testing is done for home components

---
+++ src/components/home/Testing/Searchbar.test.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Searchbar from './../Searchbar';
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import {clearInput} from '../Searchbar';
+import { Grid } from '@material-ui/core';
+
+
+configure({adapter: new Adapter()});
+
+describe('<Searchbar />' , () => {
+
+    let wrapper;
+    
+    let theme={};    
+    let destination = [
+        {
+          
+          "language": "English",
+          "link": "https://en.wikipedia.org/wiki/Sigiriya\n",      
+          "title": "sigiriya",
+          "year": 1958
+        },
+        {
+          
+          "language": "English",
+          "link": "/destination",      
+          "title": "Sinharaja Rain Forest",
+          "year": 1836
+        },
+        
+      ];
+
+    beforeEach(() => {
+                              
+        wrapper = shallow(<ThemeProvider theme={theme}><Searchbar /></ThemeProvider>);
+    });
+    
+
+    // it('should update state as expected when clearInput method executed' , () =>{
+    //     console.log(wrapper.debug());
+    //     expect(wrapper.state('wordEntered')).toEqual("");
+    //     const instance = wrapper.dive().instance();
+    // const spy = jest.spyOn(instance, 'filteredData');
+    // const spyon = instance.clearInput();
+    // expect(spyon).toHaveBeenCalledTimes(0);
+
+    
+    // });
+    it('should render  Searchbar Component', () => {  
+        expect(wrapper).toBeTruthy();
+        //expect(wrapper.find(Grid)).toHaveLength(1);
+        
+    });
+});;
---
+++ src/components/home/Testing/Upperbar.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Upperbar from '../Upperbar';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+
+configure({adapter: new Adapter()});
+
+describe('<Upperbar />', () => {
+
+    let wrapper; 
+    const theme = {} ;
+
+    beforeEach(() => {        
+        wrapper = shallow(<ThemeProvider theme={theme}><Upperbar/></ThemeProvider>);                
+    });
+
+    it('should matches the snapshot',() => {
+        expect(wrapper.getElements()).toMatchSnapshot();
+    });
+          
+    
+    
+});
